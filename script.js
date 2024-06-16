7d426123279bf728e9e3da4451e85d374adc7f35
adityanarayanan343 <adityanarayanan343@gmail.com>
2024-06-16 11:28:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let display = document.getElementById('display');
+
+function appendCharacter(char) {
+    if (display.innerText === '0') {
+        display.innerText = char;
+    } else {
+        display.innerText += char;
+    }
+}
+
+function clearDisplay() {
+    display.innerText = '0';
+}
+
+function deleteLast() {
+    if (display.innerText.length === 1) {
+        display.innerText = '0';
+    } else {
+        display.innerText = display.innerText.slice(0, -1);
+    }
+}
+
+function calculateResult() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch (e) {
+        display.innerText = 'Error';
+    }
+}
